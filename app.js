cfea770f3a51f84d68e9bf381a02683c85d347fb
TOOBA ZAFAR <160070906+toobazafar@users.noreply.github.com>
2024-08-02 22:02:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+var languages = ['english', 'urdu', 'hindi', 'french', 'arabic'];
+console.log("List of Languages:");
+for (var _i = 0, languages_1 = languages; _i < languages_1.length; _i++) {
+    var top_1 = languages_1[_i];
+    console.log(top_1);
+}
---
+++ app.ts
@@ -0,0 +1,5 @@
+const languages : string [] = ['english','urdu','hindi','french','arabic']
+console.log("List of Languages:")
+for(let top of languages){
+console.log(top)
+}
